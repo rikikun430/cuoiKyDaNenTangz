5c50916da447b8f4e9ec71e95a7ff03bd408ce96
rikikun430 <114466475+rikikun430@users.noreply.github.com>
2023-05-06 19:09:51 +0700

    Add files via upload

--- note.dart
+++ note.dart
@@ -1,101 +1,27 @@
-import 'package:code/src/constants/color.dart';
-import 'package:code/src/constants/sizes.dart';
-import 'package:code/src/feature/MainScreen/models/note.dart';
-import 'package:code/src/feature/MainScreen/models/Task.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:intl/intl.dart';
+class Note {
+  String? title;
+  String? content;
+  DateTime? date;
 
-class NoteField extends StatelessWidget {
-  final Note notes;
-  const NoteField({required this.notes});
-
-  @override
-  Widget build(BuildContext context) {
-    final DateFormat _dateFormatter = DateFormat('dd MMM');
-    final DateFormat _timeFormatter = DateFormat('h:mm');
-    final now = DateTime.now();
-    return Container(
-      height: 300,
-      child: Expanded(
-        child: ListView(
-          scrollDirection: Axis.vertical,
-          padding: const EdgeInsets.all(8),
-          children: <Widget>[
-            SizedBox(height: 20.0),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 30.0),
-              padding: EdgeInsets.all(30.0),
-              decoration: BoxDecoration(
-                color: Color(0xFFF5F7FB),
-                borderRadius: BorderRadius.circular(30.0),
-              ),
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        notes.title ?? "asdasda",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      Text(
-                        _timeFormatter.format(notes.date ?? now),
-                        style: TextStyle(
-                          color: Color(0xFFAFB4C6),
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 15.0),
-                  Text(
-                    notes.content ?? "Content Default",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: <Widget>[
-                      Text(
-                        _dateFormatter.format(notes.date ?? now),
-                        style: TextStyle(
-                          color: Color(0xFFAFB4C6),
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      Container(
-                        height: 50.0,
-                        width: 50.0,
-                        decoration: BoxDecoration(
-                          color: Color(0xFF417BFB),
-                          borderRadius: BorderRadius.circular(15.0),
-                        ),
-                        child: Icon(
-                          Icons.location_on,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+  Note({this.title, this.content, this.date});
 }
+
+final Map<String, int> categories = {
+  'Notes': 112,
+  'Work': 58,
+  'Home': 23,
+  'Complete': 31,
+};
+
+final List<Note> notes = [
+  Note(
+    title: 'Buy ticket',
+    content: 'Buy airplane ticket through Kayak for \$318.38',
+    date: DateTime(2019, 10, 10, 8, 30),
+  ),
+  Note(
+    title: 'Walk with dog',
+    content: 'Walk on the street with my favorite dog.',
+    date: DateTime(2019, 10, 10, 8, 30),
+  ),
+];
